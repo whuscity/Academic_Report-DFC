5102e77585d9e83aa59f37fea0235186ae53b0b4
MarioZZJ <jack.z-517@outlook.com>
2019-11-18 18:51:13 +0800

    save的更新（1116） 1.函数加必传参数self； 2.本机mysql配置设变量保存，今后需要更改时只需要在开头更改一次； 3.sql语句适配1116版本，字段已部分更改的数据库，不再直接插入，而是指定字段插入。

--- crawler/save_to_mysql.py
+++ crawler/save_to_mysql.py
@@ -1,56 +1,61 @@
 import pymysql
 #将需要单独处理的动态页面信息存入数据库
+hostD = 'localhost'
+portD = 3306
+userD = 'crawler001'
+passwdD = '123456'
+dbD = 'acarep_crawler'
+charsetD = 'utf8mb4'
 
 class save_to_database(object):
-    def save_dynamicURL(data_list):
-        conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='123456', db='test', charset='utf8')  # 基本的本机MYSQL配置
+    def save_dynamicURL(self,data_list):
+        conn = pymysql.connect(host=hostD, port=portD, user=userD, passwd=passwdD, db=dbD, charset=charsetD)  # 基本的本机MYSQL配置
         cursor = conn.cursor()
-        sql = "insert into dynamic_urls VALUES (%s,%s,%s,%s,%s)"
+        sql = "insert into dynamic_urls (university,school,first_page,second_page,detail_model) VALUES (%s,%s,%s,%s,%s)"
         cursor.execute(sql, data_list)
         conn.commit()
         conn.close()
 
     #将入口网址为空的院校存入数据库
-    def save_emptyURL(data_list):
-        conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='123456', db='test', charset='utf8')  # 基本的本机MYSQL配置
+    def save_emptyURL(self,data_list):
+        conn = pymysql.connect(host=hostD, port=portD, user=userD, passwd=passwdD, db=dbD, charset=charsetD)  # 基本的本机MYSQL配置
         cursor = conn.cursor()
-        sql = "insert into empty_urls VALUES (%s,%s,%s,%s,%s)"
+        sql = "insert into empty_urls (university,school,first_page,second_page,detail_model) VALUES (%s,%s,%s,%s,%s)"
         cursor.execute(sql, data_list)
         conn.commit()
         conn.close()
 
     #将无法打开的入口链接信息存入数据库
-    def save_refusedURLS(data_list):
-        conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='123456', db='test', charset='utf8')  # 基本的本机MYSQL配置
+    def save_refusedURLS(self,data_list):
+        conn = pymysql.connect(host=hostD, port=portD, user=userD, passwd=passwdD, db=dbD, charset=charsetD)  # 基本的本机MYSQL配置
         cursor = conn.cursor()
-        sql = "insert into refused_enter_urls VALUES (%s,%s,%s,%s,%s)"
+        sql = "insert into refused_enter_urls (university,school,first_page,second_page,detail_model) VALUES (%s,%s,%s,%s,%s)"
         cursor.execute(sql, data_list)
         conn.commit()
         conn.close()
 
     #将列表页的信息存入数据库
-    def save_listURLs(data_list):
-        conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='123456', db='test', charset='utf8')  # 基本的本机MYSQL配置
+    def save_listURLs(self,data_list):
+        conn = pymysql.connect(host=hostD, port=portD, user=userD, passwd=passwdD, db=dbD, charset=charsetD)  # 基本的本机MYSQL配置
         cursor = conn.cursor()
-        sql = "insert into list_urls VALUES (%s,%s,%s,%s)"
+        sql = "insert into list_urls (university,school,list_url,detail_model) VALUES (%s,%s,%s,%s)"
         cursor.executemany(sql, data_list)
         conn.commit()
         conn.close()
 
-    def save_error_info(data_list):
-        conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='123456', db='test', charset='utf8')  # 基本的本机MYSQL配置
+    def save_error_info(self,data_list):
+        conn = pymysql.connect(host=hostD, port=portD, user=userD, passwd=passwdD, db=dbD, charset=charsetD)  # 基本的本机MYSQL配置
         cursor = conn.cursor()
-        sql = "insert into error_info VALUES (%s,%s,%s)"
+        sql = "insert into error_info (university,school,academic_url) VALUES (%s,%s,%s)"
         cursor.executemany(sql, data_list)
         conn.commit()
         conn.close()
 
     #将详情页的信息存入数据库
-    def save_academic_info(data_list):
-        conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='123456', db='test',
-                               charset='utf8')  # 基本的本机MYSQL配置
+    def save_academic_info(self,data_list):
+        conn = pymysql.connect(host=hostD, port=portD, user=userD, passwd=passwdD, db=dbD, charset=charsetD)  # 基本的本机MYSQL配置
         cursor = conn.cursor()
-        sql = "insert into academic_info VALUES (%s,%s,%s,%s)"
+        sql = "insert into academic_info (university,school,academic_url,academic_url,detail_text) VALUES (%s,%s,%s,%s)"
         cursor.executemany(sql, data_list)
         conn.commit()
         conn.close()
