ef40e908c052f30a00078732b2fcf842d0439060
MarioZZJ <jack.z-517@outlook.com>
2020-11-09 21:35:31 +0800

    增加时间正则匹配以及转化到ann标注文件

---
+++ extraction/db_operation/getdata.py
@@ -0,0 +1,10 @@
+import pymysql
+from db_operation.getdb import get_db
+
+def get_data(sql):
+    hostD, portD, userD, passwdD, dbD, charsetD = get_db()
+    conn = pymysql.connect(host=hostD, port=portD, user=userD, passwd=passwdD, db=dbD, charset=charsetD)
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    return data
--- extraction/tagging/record_to_txt.py
+++ extraction/tagging/record_to_txt.py
@@ -1,19 +1,11 @@
-import pymysql,os
-# 数据库连接信息配置
-hostD = 'localhost'
-portD = 3306
-userD = 'PM'
-passwdD = '888888'
-dbD = 'AcademicReport'
-charsetD = 'utf8'
+import pymysql,os,sys
+sys.path.append("..")
+import db_operation.getdata as d
 
 # 连接数据库并取出信息
 def get_data(step):
-    conn = pymysql.connect(host=hostD, port=portD, user=userD, passwd=passwdD, db=dbD, charset=charsetD)
-    cursor = conn.cursor()
     sql = 'select id,url_id,detail_text from academic_info LIMIT ' + str(step) + ', 1000'
-    cursor.execute(sql)
-    data = cursor.fetchall()
+    data = d.get_data(sql);
     return data
 
 def save_as_txt(data):
@@ -21,10 +13,10 @@ def save_as_txt(data):
         id = row[0]
         school_id = ("000"+str(row[1]))[-4:]
         text = row[2]
-        path = '.data/' + str(school_id)
+        path = './data/' + str(school_id)
         if os.path.exists(path):pass
         else:os.makedirs(path)
-        with open((path + '/' + str(school_id) + "-" + str(id)+".txt"),'w',encoding=charsetD) as file:
+        with open((path + '/' + str(school_id) + "-" + str(id)+".txt"),'w',encoding='UTF-8') as file:
             file.write(text)
             file.close()
             print("\r已保存 "+str(school_id)+"-"+str(id)+".txt",end="")
@@ -32,4 +24,3 @@ def save_as_txt(data):
 for i in range(324):
     data = get_data(i*1000)
     save_as_txt(data)
-
